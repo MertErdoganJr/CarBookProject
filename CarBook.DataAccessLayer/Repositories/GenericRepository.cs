478cd77247943c83b9577664b06a0dee8c939f6e
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-10-09 21:05:04 +0300

    Add-Generic Repository 2

--- CarBook.DataAccessLayer/Repositories/GenericRepository.cs
+++ CarBook.DataAccessLayer/Repositories/GenericRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CarBook.DataAccessLayer.Abstract;
+using CarBook.DataAccessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,35 @@ using System.Threading.Tasks;
 
 namespace CarBook.DataAccessLayer.Repositories
 {
-    public class GenericRepository
+    public class GenericRepository<T> : IGenericDal<T> where T : class
     {
+        CarBookContext context = new CarBookContext();
+        public void Delete(T entity)
+        {
+            context.Set<T>().Remove(entity);
+            context.SaveChanges();
+        }
 
+        public T GetByID(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T entity)
+        {
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            context.Set<T>().Update(entity);
+            context.SaveChanges();
+        }
     }
 }

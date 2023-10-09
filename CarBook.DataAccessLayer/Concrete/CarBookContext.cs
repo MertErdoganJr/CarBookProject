7ac11d1accaed61d883948a2c74643234e428585
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-10-09 19:34:23 +0300

    Add-GenericRepository

--- CarBook.DataAccessLayer/Concrete/CarBookContext.cs
+++ CarBook.DataAccessLayer/Concrete/CarBookContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CarBook.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,18 @@ using System.Threading.Tasks;
 
 namespace CarBook.DataAccessLayer.Concrete
 {
-    public class CarBookContext
+    public class CarBookContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-JNJNVEQ\\MERTSQL;initial catalog=CarbookDb;integrated security=true;");
+        }
 
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarCategory> CarCategories { get; set; }
+        public DbSet<CarStatus> CarStatuses { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Price> Prices { get; set; }
     }
 }

333545d5d1288f708ead78755ed7107dc03a0e3c
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-10-19 22:20:25 +0300

    Add-Price & Car Brand

--- CarBook.DataAccessLayer/EntityFramework/EfPriceDal.cs
+++ CarBook.DataAccessLayer/EntityFramework/EfPriceDal.cs
@@ -1,6 +1,8 @@
 ﻿using CarBook.DataAccessLayer.Abstract;
+using CarBook.DataAccessLayer.Concrete;
 using CarBook.DataAccessLayer.Repositories;
 using CarBook.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,11 @@ namespace CarBook.DataAccessLayer.EntityFramework
 {
     public class EfPriceDal : GenericRepository<Price>, IPriceDal
     {
+        public List<Price> GetPricesWithCars()
+        {
+            var context = new CarBookContext();
+            var values = context.Prices.Include(x=>x.Car).ThenInclude(y=>y.Brand).ToList();
+            return values;
+        }
     }
 }

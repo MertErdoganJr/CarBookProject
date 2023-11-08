975f582c98dde47d865d462f0f731d750b4dfa7f
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-11-08 19:44:48 +0300

    Add - Car Details Page

--- CarBook.PresentationLayer/ViewComponents/CarDetailComponents/_CarDetailCategoryComponentPartial.cs
+++ CarBook.PresentationLayer/ViewComponents/CarDetailComponents/_CarDetailCategoryComponentPartial.cs
@@ -1,20 +1,26 @@
 ﻿using CarBook.BusinessLayer.Abstract;
 using CarBook.DataAccessLayer.Abstract;
+using CarBook.DataAccessLayer.Concrete;
+using CarBook.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarBook.PresentationLayer.ViewComponents.CarDetailComponents
 {
     public class _CarDetailCategoryComponentPartial : ViewComponent
     {
+        
         private readonly ICarCategoryService _carCategoryService;
 
         public _CarDetailCategoryComponentPartial(ICarCategoryService carCategoryService)
         {
+
             _carCategoryService = carCategoryService;
         }
 
         public IViewComponentResult Invoke()
         {
+           
             var values = _carCategoryService.TGetListAll();
             return View(values);
         }
